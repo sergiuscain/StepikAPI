dc66866189b5ed51c2461011dccd473a1f07ae20
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-12-10 08:48:41 +0300

    Test api in console

--- Controllers/CertificatesController.cs
+++ Controllers/CertificatesController.cs
@@ -12,7 +12,6 @@ public class CertificatesController : ControllerBase
     {
         _certificatesService = new CertificatesService();
     }
-
     [HttpGet("GetUserCertificates")]
     public IActionResult GetUserCertificates(string fullName)
     {
