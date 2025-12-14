21c4ff98586845842fa4dab02e7f39677273f1c0
Sergey Korolev <167008320+sergiuscain@users.noreply.github.com>
2025-12-14 19:13:23 +0300

    Добавил модель настройки сертефиката и дпработал модель курса

---
+++ Models/CertificateSetting.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("certificate_settings")]
+public class CertificateSetting
+{
+    [Key]
+    [Column("course_id")]
+    public int CourseId { get; set; }
+    [Column("logo_url")]
+    public string LogoUrl { get; set; }
+    [Column("signature_url")]
+    public string SignatureUrl { get; set; }
+    [Column("is_certificate_auto_issued")]
+    public bool IsCertificateAutoIssued { get; set; }
+    [Column("regular_threshold")]
+    public int RegularThreshold { get; set; }
+    [Column("excellent_threshold")]
+    public int ExcellentThreshold { get; set; }
+
+    public Course Course { get; set; }
+}
--- Models/Course.cs
+++ Models/Course.cs
@@ -1,7 +1,27 @@
-﻿public class Course
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("courses")]
+public class Course
 {
+    [Column("id")]
     public int Id { get; set; }
-    public string Title { get; set; } = default!;
+
+    [Column("title")]
+    [StringLength(50)]
+    public string Title { get; set; }
+
+    [Column("created_date")]
+    public DateTime CreatedDate { get; set; }
+
+    [Column("summary")]
     public string? Summary { get; set; }
+
+    [Column("photo")]
     public string? Photo { get; set; }
+
+    [Column("price")]
+    public decimal Price { get; set; }
+
+    public CertificateSetting CertificateSetting { get; set; }
 }
