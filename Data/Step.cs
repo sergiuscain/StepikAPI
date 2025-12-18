674e182f954fd40fce6497685b1ea34f05fe9473
Sergey Korolev <167008320+sergiuscain@users.noreply.github.com>
2025-12-18 17:20:32 +0300

    Удалил модели, созданные по ошибке

--- Data/Step.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace API.Data
-{
-    public class Step
-    {
-    }
-}
--- Models/Lesson.cs
+++ Models/Lesson.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 [Table("lessons")]
 public class Lesson
